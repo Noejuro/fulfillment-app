9b6500180ac6469b93b12b6fcb89bc7fb80d67df
Noé <noe_juarez@epam.com>
2022-03-02 21:32:20 -0600

    [Init] Project Initialization

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <h1>Fulfillment App</h1>
+    </BrowserRouter>
+  );
+}
+
+export default App;
